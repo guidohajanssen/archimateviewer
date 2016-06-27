95700d27962aed8785e638508cf3fdf8dd2ded87
Guido Janssen <guido.janssen@pharmapartners.nl>
2016-06-27 22:19:11 +0200

    Adjusted batch files for better input and output locations.

--- publisharchimatetool.cmd
+++ publisharchimatetool.cmd
@@ -1,8 +1,8 @@
-mkdir open-exchange
-java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/archimatetool.xsl -s:models/%1.archimate -o:open-exchange/%1.xml
-rmdir /s /q %2
-mkdir %2
-java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/index.xsl -s:open-exchange/%1.xml -o:%2/index.html
-java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/browsepages.xsl -s:open-exchange/%1.xml folder=%2
-mkdir %2\lib
-xcopy lib\reportlibs %2 /s /e
+mkdir output\open-exchange 
+java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/archimatetool.xsl -s:%1 -o:output\open-exchange/%2.xml
+rmdir /s /q %2 
+mkdir output\site\%2 
+java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/index.xsl -s:output\open-exchange/%2.xml -o:output\site\%2/index.html
+java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/browsepages.xsl -s:output\open-exchange/%2.xml folder=output\site\%2
+mkdir output\site\%2\lib 
+xcopy lib\reportlibs output\site\%2 /s /e
