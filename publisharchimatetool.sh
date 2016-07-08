edcad8aefa9fe0d47e897651b326c992a06da53c
Guido Janssen <guidohajanssen@gmail.com>
2016-07-08 21:28:45 +0200

    Fixed command line

--- publisharchimatetool.sh
+++ publisharchimatetool.sh
@@ -11,4 +11,4 @@ mkdir -p output/open-exchange
 echo "generating open group exchange format file output/open-exchange/"$2".xml for "$1
 java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/archimatetool.xsl -s:$1 -o:output/open-exchange/$2.xml
 
-./	publishopengroup.sh output/open-exchange/$2.xml $2 $3 $4
+./publishopengroup.sh output/open-exchange/$2.xml $2 $3 $4
