9e1f4298d2fc5504e79bd5d2892a69d799bfa947
Guido Janssen <guido.janssen@pharmapartners.nl>
2016-07-29 11:08:55 +0200

    Fixed command files for generation.

--- publisharchimatetool.cmd
+++ publisharchimatetool.cmd
@@ -1,18 +1,5 @@
 mkdir output\open-exchange 
-java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/archimatetool.xsl -s:%1 -o:output\open-exchange/%2.xml
+java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/archimatetool.xsl -s:%1 -o:output\open-exchange\openexchange.xml
 
-rmdir /s /q output\xsl
-mkdir output\xsl
-xcopy xsl\default.xsl output\xsl\default.xsl
-java -jar lib/java/saxon/saxon9he.jar -xsl:xsl\preprocess.xsl -s:xsl\preindex.xsl -o:output\xsl\index.xsl customxsl=%4
-java -jar lib/java/saxon/saxon9he.jar -xsl:xsl\preprocess.xsl -s:xsl\prebrowsepages.xsl -o:output\xsl\browsepages.xsl customxsl=%4
+publishopengroup output\open-exchange\openexchange.xml %2 %3 %4
 
-rmdir /s /q %2 
-mkdir output\site\%2 
-
-java -jar lib\java\saxon\saxon9he.jar -xsl:output\xsl\index.xsl -s:output\open-exchange\%2.xml -o:output/site/%2/index.html
-
-java -jar lib\java\saxon\saxon9he.jar -xsl:output\xsl\browsepages.xsl -s:output\open-exchange\%2.xml folder=output/site/%2 configfile=%3
-
-mkdir output\site\%2\lib 
-xcopy lib\reportlibs output\site\%2 /s /e /y
--- publishopengroup.cmd
+++ publishopengroup.cmd
@@ -1,6 +1,13 @@
-rmdir output\site\%2 /s /q 
-mkdir output\site\%2
-java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/index.xsl -s:%1 -o:output\site\%2/index.html
-java -jar lib/java/saxon/saxon9he.jar -xsl:xsl/browsepages.xsl -s:%1 folder=output\site\%2 configfile=%3
-mkdir output\site\%2\lib
-xcopy lib\reportlibs output\site\%2 /s /e /y
+
+rmdir output\xsl /s /q
+mkdir output\xsl
+copy xsl\default.xsl output\xsl\default.xsl 
+java -jar lib/java/saxon/saxon9he.jar -xsl:xsl\preprocess.xsl -s:xsl\preindex.xsl -o:output\xsl\index.xsl customxsl=%4
+java -jar lib/java/saxon/saxon9he.jar -xsl:xsl\preprocess.xsl -s:xsl\prebrowsepages.xsl -o:output\xsl\browsepages.xsl customxsl=%4
+
+rmdir %2 /s /q 
+mkdir %2
+java -jar lib/java/saxon/saxon9he.jar -xsl:output\xsl\index.xsl -s:%1 -o:%2\index.html
+java -jar lib/java/saxon/saxon9he.jar -xsl:output\xsl\browsepages.xsl -s:%1 folder=%2 configfile=%3
+mkdir %2\lib
+xcopy lib\reportlibs %2 /s /e /y
