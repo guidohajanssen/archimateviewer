d3ac354e734aea3c2705e896742d6051642672fe
Guido Janssen <guidohajanssen@gmail.com>
2016-08-27 10:24:55 +0200

    Fixed command for replacetype.xsl

--- publishopengroup.cmd
+++ publishopengroup.cmd
@@ -7,7 +7,6 @@ java -jar lib/java/saxon/saxon9he.jar -xsl:xsl\preprocess.xsl -s:xsl\prebrowsepa
 
 rmdir %2 /s /q 
 mkdir %2
-java -jar lib/java/saxon/saxon9he.jar -xsl:output\xsl\index.xsl -s:%1 -o:%2\index.html
-java -jar lib/java/saxon/saxon9he.jar -xsl:output\xsl\browsepages.xsl -s:%1 folder=%2 configfile=%3
-mkdir %2\lib
-xcopy lib\reportlibs %2 /s /e /y
+java -jar lib/java/saxon/saxon9he.jar -xsl:xsl\replacetype.xsl -s:%1 -o:output\model.xml
+java -jar lib/java/saxon/saxon9he.jar -xsl:output\xsl\index.xsl -s:output\model.xml -o:%2\index.html
+java -jar lib/java/saxon/saxon9he.jar -xsl:output\xsl\browsepages.xsl -s:output\model.xml folder=%2 configfile=%3
